79d25f59636d1ea6cda8f266d488ddeaa86d8319
versatile-bug-8498 <dustin.clausing@gmail.com>
2019-02-04 05:18:44 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -9,4 +9,6 @@ describe "current_age_for_birth_year method" do
     age_of_person = current_age_for_birth_year (birth_year)
     expect (age_of_person) .to eq(answer)
   end
-  
+ def current_age_for_birth_year (birth_year)
+   2003 - birth_year
+ end
